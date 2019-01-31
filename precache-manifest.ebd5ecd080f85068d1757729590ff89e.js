44165ccd6aa208542e9aea2f5736e55b376edd35
root <root@localhost.localdomain>
2019-02-01 00:35:25 +0330

    Updates

---
+++ precache-manifest.ebd5ecd080f85068d1757729590ff89e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "aac43a4ccda5387f152a",
+    "url": "/maktabkhone-resume/static/css/main.a8fbcd26.chunk.css"
+  },
+  {
+    "revision": "aac43a4ccda5387f152a",
+    "url": "/maktabkhone-resume/static/js/main.aac43a4c.chunk.js"
+  },
+  {
+    "revision": "373adefa9f47a8d07de8",
+    "url": "/maktabkhone-resume/static/js/1.373adefa.chunk.js"
+  },
+  {
+    "revision": "2f0f67a8de50454842d1",
+    "url": "/maktabkhone-resume/static/js/runtime~main.2f0f67a8.js"
+  },
+  {
+    "revision": "bc4ad19c2f87f45db1989d6f4d635ff8",
+    "url": "/maktabkhone-resume/index.html"
+  }
+];
